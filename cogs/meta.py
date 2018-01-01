2764294ee0281595501297dc8f8de1581319bb81
Perry Fraser <perry@frasers.org>
2018-01-01 10:10:42 -0500

    Learn how to do math

--- cogs/meta.py
+++ cogs/meta.py
@@ -178,7 +178,7 @@ class Meta:
 
     @commands.command(aliases=['pong'])
     async def ping(self, ctx):
-        await ctx.send('Pong! {:,.2}ms of latency! 🏓'.format(self.bot.latency))
+        await ctx.send(f'Pong! {self.bot.latency*1000:,.2}ms of latency! 🏓')
 
     @commands.command(aliases=['fb'])
     @commands.cooldown(rate=1, per=5, type=commands.BucketType.user)
