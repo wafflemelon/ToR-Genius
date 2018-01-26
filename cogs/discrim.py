8f75d3e35a8c1219bc24679100730d48b4b97f9e
Perry Fraser <perry@frasers.org>
2018-01-26 15:53:49 -0500

    fix last commit and stuff

--- cogs/discrim.py
+++ cogs/discrim.py
@@ -21,21 +21,10 @@ class Discrim:
         count = Counter(discrim_list + [int(i) for i in range(1, 10000)])
         count = sorted(count.items(), key=lambda c: c[1], reverse=True)
 
-        data = {
-            'Most ': ', '.join(str(d[0]) for d in count[:5]) + ', and ' + str(count[6][0]),
-            'least common': ', '.join(str(d[0]) for d in count[-4:-1][::-1]) + ', and' +
-                            str(count[-1][0]),
-            'mean': np.mean(discrim_list),
-            'mode': stats.mode(discrim_list),
-            'median': np.median(discrim_list),
-            'Standard Deviation': np.std(discrim_list),
-            'unused': '\n'.join([str(d) for d in count if d[1] == 1][:10])
-        }
-
         embeds = {
             'Summary': {
                 'Most Common': ', '.join(str(d[0]) for d in count[:3]) + ', and ' + str(count[4][0]),
-                'Least Common': ', '.join(str(d[0]) for d in count[-4:-1][::-1] if d[1] != 1) + ', and ' + str(count[-1][0]),
+                'Least Common': ', '.join(str(d[0]) for d in count[-4:-1][::-1]) + ', and ' + str(count[-1][0]),
                 'Three Unused': '\n'.join([str(d[0]) for d in count if d[1] == 1][:3]),
                 'Average': np.mean(discrim_list),
             },
@@ -46,16 +35,6 @@ class Discrim:
                 'Standard Deviation': np.std(discrim_list),
             }
         }
-        #
-        # e = discord.Embed(
-        #     title="Embed Info",
-        #     color=ctx.author.top_role.color
-        # )
-        #
-        # for fieldName in data.keys():
-        #     e.add_field(name=fieldName, value=data[fieldName])
-        #
-        # await ctx.send(embed=e)
 
         final_embeds = []
 
@@ -66,19 +45,6 @@ class Discrim:
             final_embeds.append(e)
 
         p = EmbedPages(ctx, embeds=final_embeds)
-        await p.paginate()
-
-    @commands.command()
-    async def tmp(self, ctx):
-        embeds = [
-            discord.Embed(title="foo bar"),
-            discord.Embed(title="foo bar1"),
-            discord.Embed(title="foo bar2"),
-            discord.Embed(title="foo bar3"),
-            discord.Embed(title="foo bar4")
-        ]
-
-        p = EmbedPages(ctx, embeds=embeds)
         await p.paginate()
 
 
