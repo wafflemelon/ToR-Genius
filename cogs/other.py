8c692dd3ecc4bb4c28eafd971e0e02ce214fe2cf
Perry Fraser <perry@frasers.org>
2018-01-30 20:33:18 -0500

    THE BEST FEATURE YET
    ya bad formatting will fix later

--- cogs/other.py
+++ cogs/other.py
@@ -1,4 +1,10 @@
+import io
+import re
+from math import floor
+
 import aiohttp
+import discord
+from PIL import Image, ImageFont, ImageDraw
 from discord.ext import commands
 
 from cogs.utils.paginator import Pages
@@ -52,6 +58,75 @@ class Other:
                 )
 
                 await p.paginate()
+
+    @staticmethod
+    async def download(url):
+        async with aiohttp.ClientSession() as sess:
+            async with sess.get(url) as r:
+                return io.BytesIO(await r.read())
+
+    @commands.command()
+    async def blame(self, ctx, *, member: commands.MemberConverter = None):
+        # hardcoded because I want to be blamed even in forks ;)
+        member = member or self.bot.get_user(280001404020588544)
+        if member is None:
+            return await ctx.send("No user specified, and owner couldn't "
+                                  "be found")
+
+        # :no_entry: emoji
+        emoji = 'https://emojipedia-us.s3.amazonaws.com/thumbs/240/twitter/' \
+                '120/no-entry-sign_1f6ab.png'
+        emoji = await self.download(emoji)
+        emoji = Image.open(emoji)
+        emoji = emoji.convert('RGBA')
+
+        # noinspection PyUnresolvedReferences
+        url = member.avatar_url_as(format='png')
+        url = url.replace('gif', 'png').strip('<>')
+        img = await self.download(url)
+
+        img = Image.open(img)
+        img = img.convert('RGBA')
+
+        large_image = Image.new('RGBA', [3 * x for x in img.size], (0, 0, 0, 0))
+        box = (img.size[0], img.size[0], img.size[0] * 2, img.size[0] * 2)
+
+        emoji = emoji.resize([floor(x * 1.2) for x in img.size])
+
+        large_image.paste(img.copy(), box)
+        large_image.paste(
+            emoji,
+
+            (
+                floor((large_image.size[0] - emoji.size[0]) / 2),
+
+                floor((large_image.size[1] - emoji.size[1]) / 2)
+            ),
+
+            emoji
+        )
+
+        fnt = ImageFont.truetype('Arial.ttf', floor(img.size[0] / 4))
+        d = ImageDraw.Draw(large_image)
+
+        # noinspection PyUnresolvedReferences
+        name = re.sub(r'\W', '', member.display_name).lower()
+        message = f'#blame{name}'
+        w, h = d.textsize(message, fnt)
+
+        d.text(
+            (
+                floor((large_image.size[0] - w) / 2),
+                floor(img.size[1] / 2) - floor(img.size[0] / 4)),
+            message,
+            font=fnt,
+            fill=(255,) * 4
+        )
+
+        bio = io.BytesIO()
+        large_image.save(bio, 'PNG')
+        bio.seek(0)
+        await ctx.send(file=discord.File(bio, filename='blame.png'))
 
 
 def setup(bot):
