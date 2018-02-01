6ba0d58706765a7c8491f603ae097d7e5d724b59
Perry Fraser <perry@frasers.org>
2018-01-31 21:18:24 -0500

    awesome blame command update

--- cogs/other.py
+++ cogs/other.py
@@ -10,9 +10,69 @@ from discord.ext import commands
 from cogs.utils.paginator import Pages
 
 
+async def download(url):
+    async with aiohttp.ClientSession() as sess:
+        async with sess.get(url) as r:
+            return io.BytesIO(await r.read())
+
+
+class LinkOrAvatar(commands.Converter):
+    special_cases = {
+        'itsthejoker': 'https://avatars0.githubusercontent.com/u/5179553'
+    }
+
+    async def convert(self, ctx, argument):
+        try:
+            possible_member = await commands.MemberConverter() \
+                .convert(ctx, argument)
+            if possible_member.name not in self.special_cases:
+                url = possible_member.avatar_url_as(format='png')
+                url = url.replace('gif', 'png').strip('<>')
+            else:
+                url = self.special_cases[possible_member.name]
+
+            img = await download(url)
+
+            img = Image.open(img)
+
+            return img.convert('RGBA'), possible_member.name
+        except commands.BadArgument:
+            pass
+
+        # from https://stackoverflow.com/questions/169625/
+        # regex-to-check-if-valid-url-that-ends-in-jpg-png-or-gif
+        # (Sorry about breaking the URL)
+
+        # will add more image formats as time goes on
+        regex = r'<?(?:([^:/?#]+):)?(?://([^/?#]*))?([^?#]*\.' \
+                r'(?:jpg|png|jpeg))(?:\?([^#]*))?(?:#(.*))?>?'
+
+        regex = re.compile(regex, re.IGNORECASE)
+
+        if re.fullmatch(regex, argument.split(' ')[0]):
+            img = await download(argument.split(' ')[0].strip('<>'))
+
+            img = Image.open(img)
+
+            text = ' '.join(argument.split(' ')[1:])
+            if not text:
+                raise commands.BadArgument('No text supplied for image')
+            return img.convert('RGBA'), text
+        else:
+            raise commands.BadArgument(
+                "That URL doesn't seem to lead to a valid image"
+                # if possible_member else "I couldn't find that user"
+            )
+
+
 class Other:
     def __init__(self, bot):
         self.bot = bot
+
+    @staticmethod
+    async def __error(ctx, err):
+        if isinstance(err, commands.BadArgument):
+            await ctx.send(err)
 
     @commands.command()
     async def forum(self, ctx, *, search):
@@ -59,59 +119,34 @@ class Other:
 
                 await p.paginate()
 
-    @staticmethod
-    async def download(url):
-        async with aiohttp.ClientSession() as sess:
-            async with sess.get(url) as r:
-                return io.BytesIO(await r.read())
-
     # noinspection PyUnresolvedReferences,PyPep8Naming
     @commands.command()
-    async def blame(self, ctx, *, member: commands.MemberConverter = None):
-        """Blame everyone! Defaults to perryprog."""
-        # hardcoded because I want to be blamed even in forks ;)
-        member = member or self.bot.get_user(280001404020588544)
-        if not member:
-            return await ctx.send("No user specified, and owner couldn't "
-                                  "be found")
+    async def blame(self, ctx, *, img: LinkOrAvatar = None):
+        """Blame everyone! Defaults to perryprog.
 
-        # special cases for usernames and avatars for people with
-        # default avatars
+        Will also accept image urls ending in jpg, png, and jpeg."""
+        # hardcoded because I want to be blamed even in forks ;)
+        img, name = img or await LinkOrAvatar() \
+            .convert(ctx, '280001404020588544')
+        # special cases for usernames
         special_cases = {
-            'names': {
-                'perryprog': 'perry',
-                'itsthejoker': 'joker'
-            },
-            'avatars': {
-                'itsthejoker': 'https://avatars0.githubusercontent.com'
-                               '/u/5179553'
-            }
+            'perryprog': 'perry',
+            'itsthejoker': 'joker'
         }
 
         # :no_entry: emoji
         emoji = 'https://emojipedia-us.s3.amazonaws.com/thumbs/240/twitter/' \
                 '120/no-entry-sign_1f6ab.png'
-        emoji = await self.download(emoji)
+        emoji = await download(emoji)
         emoji = Image.open(emoji)
         emoji = emoji.convert('RGBA')
 
-        if member.name not in special_cases['avatars']:
-            url = member.avatar_url_as(format='png')
-            url = url.replace('gif', 'png').strip('<>')
-        else:
-            url = special_cases['avatars'][member.name]
-
-        img = await self.download(url)
-
-        img = Image.open(img)
-        img = img.convert('RGBA')
-
         # make the image 3 times larger than the avatar
-        large_image = Image.new('RGBA', [3 * x for x in img.size], (0,)*4)
+        large_image = Image.new('RGBA', [3 * x for x in img.size], (0,) * 4)
         lW, lH = large_image.size
         W, H = img.size
         # the center box for the avatar
-        box = (W, W, W * 2, W * 2)
+        box = (W, H, W * 2, H * 2)
 
         # make the emoji 20% bigger than the avatar
         emoji = emoji.resize([floor(x * 1.2) for x in img.size])
@@ -134,10 +169,10 @@ class Other:
         fnt = ImageFont.truetype('Arial.ttf', floor(img.size[0] / 4))
         d = ImageDraw.Draw(large_image)
 
-        if member.name not in special_cases['names'].keys():
-            name = re.sub(r'\W', '', member.name).lower()
-        else:
-            name = special_cases['names'][member.name]
+        name = special_cases.get(
+            name,
+            re.sub(r'\W', '', name).lower()
+        )
 
         message = f'#blame{name}'
         tW, tH = d.textsize(message, fnt)
