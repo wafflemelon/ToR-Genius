3fe733e231561dadaa8cb456ba56879254ff41c4
Perry Fraser <perry@frasers.org>
2018-02-01 20:39:08 -0500

    Add leaderboard for boston transcriptions and prompt context method

---
+++ cogs/bostonlib.py
@@ -0,0 +1,93 @@
+import discord
+from discord.ext import commands
+
+from cogs.utils import db
+
+
+class BostonTable(db.Table, table_name='boston'):
+    user_id = db.Column(db.Integer(big=True), primary_key=True)
+    documents = db.Column(db.Integer)
+    pages = db.Column(db.Integer)
+
+
+class Boston:
+    def __init__(self, bot):
+        self.bot = bot
+
+    @staticmethod
+    async def __local_check(ctx):
+        return ctx.guild.id in [318873523579781132, 369960111679995905]
+
+    @commands.group(aliases=['bost', 'bst', 'bt'])
+    async def boston(self, ctx):
+        """Tools for tracking boston transcriptions"""
+        if ctx.invoked_subcommand is None:
+            await ctx.show_help('boston')
+
+    @boston.command(aliases=['lb', 'top', 'all'])
+    async def leaderboard(self, ctx):
+        """View a leaderboard of the top transcriptions"""
+        query = """
+SELECT *
+FROM boston
+ORDER BY pages, documents
+LIMIT 30;"""
+
+        result = await ctx.db.fetch(query)
+
+        total_docs = sum([r['documents'] for r in result])
+        total_pages = sum([r['pages'] for r in result])
+
+        top_pages = [(r['user_id'], r['pages']) for r in result]
+
+        e = discord.Embed(
+            title='Leaderboard',
+            description=f'**Total amount of documents transcribed:** '
+                        f'{total_docs}\n**Total amount of pages transcribed**:'
+                        f' {total_pages}',
+            color=ctx.author.top_role.color
+
+        )
+
+        leaderboard = []
+
+        for user, count in top_pages:
+            user = ctx.guild.get_member(user)
+            leaderboard.append(
+                f'{"Unknown" if not user else user.display_name}: {count}'
+            )
+
+        if not leaderboard:
+            return await ctx.send('No transcriptions yet.')
+
+        e.add_field(name='Top transcribers', value='\n'.join(leaderboard))
+        await ctx.send(embed=e)
+
+    @boston.command(aliases=['a'])
+    async def add(self, ctx, pages: int, documents: int = 1):
+        """Add your pages/documents"""
+        query = """
+INSERT INTO boston (user_id, documents, pages) VALUES ($1, $2, $3)
+ON CONFLICT (user_id)
+  DO UPDATE SET
+    documents = boston.documents + EXCLUDED.documents,
+    pages     = boston.pages + EXCLUDED.pages;
+        """
+
+        res = await ctx.prompt(
+            f'You are about to add {documents} document'
+            f'{"s" if documents > 1 else ""} and '
+            f'{pages} page{"s" if pages > 1 else ""}',
+            reacquire=False
+        )
+
+        if res is None:
+            return await ctx.send('Timed out.')
+
+        if res:
+            await ctx.db.execute(query, ctx.author.id, documents, pages)
+            await ctx.auto_react()
+
+
+def setup(bot):
+    bot.add_cog(Boston(bot))
--- cogs/utils/context.py
+++ cogs/utils/context.py
@@ -2,6 +2,8 @@
 # Modified work Copyright (c) 2017 Perry Fraser
 #
 # Licensed under the MIT License. https://opensource.org/licenses/MIT
+import asyncio
+
 import praw
 from discord.ext import commands
 
@@ -72,3 +74,92 @@ class Context(commands.Context):
         cmd = self.bot.get_command('help')
         command = command or self.command.qualified_name
         await self.invoke(cmd, command=command)
+
+    async def prompt(self, message, *, timeout=60.0, delete_after=True,
+                     reacquire=True, author_id=None, needs_mod=False):
+        """An interactive reaction confirmation dialog.
+        Parameters
+        -----------
+        message: str
+            The message to show along with the prompt.
+        timeout: float
+            How long to wait before returning.
+        delete_after: bool
+            Whether to delete the confirmation message after we're done.
+        reacquire: bool
+            Whether to release the database connection and then acquire it
+            again when we're done.
+        author_id: Optional[int]
+            The member who should respond to the prompt. Defaults to the author
+            of the Context's message.
+        needs_mod: bool
+            If the person doing the reaction needs mod perms, i.e. for
+            something that needs verified
+        Returns
+        --------
+        Optional[bool]
+            ``True`` if explicit confirm,
+            ``False`` if explicit deny,
+            ``None`` if deny due to timeout
+        """
+
+        if not self.channel.permissions_for(self.me).add_reactions:
+            await self.send(
+                'Bot does not have Add Reactions permission.'
+            )
+            return None
+
+        fmt = f'{message}\n\nReact with \N{WHITE HEAVY CHECK MARK} to ' \
+              f'confirm or \N{CROSS MARK} to deny.'
+
+        author_id = author_id or self.author.id
+        msg = await self.send(fmt)
+
+        confirm = None
+
+        def check(e, message_id, channel_id, user_id):
+            nonlocal confirm
+
+            # ugly but idc
+            if needs_mod and not self.guild.get_channel(
+                    channel_id).permissions_for(
+                    self.guild.get_member(user_id).ban_members is True):
+                return False
+
+            if message_id != msg.id or user_id != author_id:
+                return False
+
+            code_point = str(e)
+
+            if code_point == '\N{WHITE HEAVY CHECK MARK}':
+                confirm = True
+                return True
+            elif code_point == '\N{CROSS MARK}':
+                confirm = False
+                return True
+
+            return False
+
+        for emoji in ('\N{WHITE HEAVY CHECK MARK}', '\N{CROSS MARK}'):
+            await msg.add_reaction(emoji)
+
+        if reacquire:
+            await self.release()
+
+        try:
+            await self.bot.wait_for(
+                'raw_reaction_add',
+                check=check,
+                timeout=timeout
+            )
+        except asyncio.TimeoutError:
+            confirm = None
+
+        try:
+            if reacquire:
+                await self.acquire()
+
+            if delete_after:
+                await msg.delete()
+        finally:
+            return confirm
